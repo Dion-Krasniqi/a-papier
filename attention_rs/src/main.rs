ea3686a832bfe57ab250436e4047888e221b78d7
Dion Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2026-03-12 15:38:55 +0000

    Implemented opp, but problems with the borrowing so new type but still messy rn

--- attention_rs/src/main.rs
+++ attention_rs/src/main.rs
@@ -1,36 +1,68 @@
 use std::ops::{Add, Mul};
+use std::fmt;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
-    let a = Val::new(4.0);
-    let b = Val::new(2.0);
-    let c = a * b;
+    let a: ValRef = Rc::new(RefCell::new(Val::new(4.0)));
+    let b: ValRef = Rc::new(RefCell::new(Val::new(4.0)));
+    let c = a + b;
     println!("{}", c.data );
+    println!("{}", c.opp);
+
 }
 
 
 
 // move this to its own file but here for now
-pub struct Val {
+
+enum Op {
+    Add,
+    Mul,
+    Leaf,
+}
+impl fmt::Display for Op{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Op::Add => write!(f, "+"),
+            Op::Mul => write!(f, "*"),
+            _ => write!(f, ""),
+        }
+    }
+}
+
+type ValRef = Rc<RefCell<Val>>;
+struct Val {
     data: f32,
-    grad: f32
+    grad: f32,
+    opp: Op,
+    children: Vec<ValRef>, 
+    // the naming kinda confusing to me, also type so its mut and shared 
+                                    
 }
 
 impl Val {
     fn new(value: f32) -> Val {
         Val {
-            data: value,
-            grad: 0.0,
+                data: value,
+                grad: 0.0,
+                opp: Op::Leaf,
+                children: Vec::new(),
         }
     }
 }
 
-impl Add for Val {
-    type Output = Val;
-    fn add(self, other: Val) -> Val {
-        Val {
-            data: self.data + other.data,
-            grad: 0.0,
-        }
+impl Add for ValRef {
+    type Output = ValRef;
+    fn add(self, other: ValRef) -> ValRef {
+        Rc::new(RefCell::new(
+            Val {
+                data: self.borrow().data + other.borrow().data,
+                grad: 0.0,
+                opp: Op::Add,
+                children: vec![Rc::clone(&self), Rc::clone(&other)],
+            }
+        ))
     }
 }
 
@@ -40,6 +72,8 @@ impl Mul for Val {
         Val {
             data: self.data * other.data,
             grad: 0.0,
+            opp: Op::Mul,
+            children: self.children,
         }
     }
 }
